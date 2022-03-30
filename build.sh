9693c56a4bdaa863271632020e4c9b71368e8840
David <david_tjokroaminoto@tech.gov.sg>
2022-03-30 11:33:32 +0800

    fix: use buildx to build for amd64

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 EPOCH=$(date +%s)
 
-docker build -t 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:latest .
+docker buildx build --platform linux/amd64 -t 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:latest .
 docker push 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:latest
 
-docker build -t 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:$EPOCH .
+docker buildx build --platform linux/amd64 -t 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:$EPOCH .
 docker push 722747748666.dkr.ecr.ap-southeast-1.amazonaws.com/notarise-mockpass:$EPOCH
 
 aws ecs update-service --cluster notarise-mockpass --service arn:aws:ecs:ap-southeast-1:722747748666:service/notarise-mockpass/notarise-mockpass-service --force-new-deployment
